837d080e05472eb45294e86ed557ad3e04b1e1ba
Léo Torrez <leo@leotorrez.com.br>
2020-01-29 01:27:46 -0300

    Add husky

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const teste = 'teste';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {teste}
           Edit
           <code>src/App.js</code>
           and save to reload.
